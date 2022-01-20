586854e7aad66326272d2690f2e3e067d653948c
sylwestergladki <sylwester.gladki@gmail.com>
2022-01-20 11:02:36 +0100

    add Car model and enum FuelType

---
+++ src/main/java/io/github/sylwestergladki/carrental/model/Car.java
@@ -0,0 +1,36 @@
+package io.github.sylwestergladki.carrental.model;
+
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank(message = "Car's model must not be empty")
+    private String model;
+    @NotBlank(message = "Car's brand must not be empty")
+    private String brand;
+    @NotBlank(message = "Car's color must not be empty")
+    private String color;
+    @NotBlank(message = "Car's fuelType must not be empty")
+    private FuelType fuelType;
+
+
+
+
+
+}
---
+++ src/main/java/io/github/sylwestergladki/carrental/model/FuelType.java
@@ -0,0 +1,9 @@
+package io.github.sylwestergladki.carrental.model;
+
+public enum FuelType {
+        DIESEL,
+        PETROL,
+        LPG,
+        ELECTRIC
+}
+
