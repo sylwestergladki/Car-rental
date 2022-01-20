fa88f43c74149af9192cb5ab874c1f12d7b7043a
sylwestergladki <sylwester.gladki@gmail.com>
2022-01-20 10:54:15 +0100

    spring boot template

---
+++ src/test/java/io/github/sylwestergladki/carrental/CarRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sylwestergladki.carrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
